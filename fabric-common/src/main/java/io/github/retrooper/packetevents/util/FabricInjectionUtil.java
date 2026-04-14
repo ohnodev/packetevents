c2fcbbcff98daf728c3aa5f42da6783f7ceba28e
root <jmpowell1337@gmail.com>
2026-04-14 16:38:54 +0000

    added a filter to only add pre via encoders/decoders if there's via in the pipeline

--- fabric-common/src/main/java/io/github/retrooper/packetevents/util/FabricInjectionUtil.java
+++ fabric-common/src/main/java/io/github/retrooper/packetevents/util/FabricInjectionUtil.java
@@ -53,8 +53,11 @@ public class FabricInjectionUtil {
         String encoderName = channel.pipeline().names().contains("outbound_config") ? "outbound_config" : "encoder";
         channel.pipeline().addBefore(encoderName, PacketEvents.ENCODER_NAME, new PacketEncoder(pipelineSide, user, false));
         if (PacketEvents.getAPI().getSettings().isPreViaInjection() && ViaVersionUtil.isAvailable(user)) {
-            channel.pipeline().addBefore(VIA_DECODER_NAME, "pre-" + PacketEvents.DECODER_NAME, new PacketDecoder(pipelineSide, user, true));
-            channel.pipeline().addBefore(VIA_ENCODER_NAME, "pre-" + PacketEvents.ENCODER_NAME, new PacketEncoder(pipelineSide, user, true));
+            List<String> names = channel.pipeline().names();
+            if (names.contains(VIA_DECODER_NAME) && names.contains(VIA_ENCODER_NAME)) {
+                channel.pipeline().addBefore(VIA_DECODER_NAME, "pre-" + PacketEvents.DECODER_NAME, new PacketDecoder(pipelineSide, user, true));
+                channel.pipeline().addBefore(VIA_ENCODER_NAME, "pre-" + PacketEvents.ENCODER_NAME, new PacketEncoder(pipelineSide, user, true));
+            }
         }
         channel.closeFuture().addListener((ChannelFutureListener) future ->
                 PacketEventsImplHelper.handleDisconnection(user.getChannel(), user.getUUID()));
